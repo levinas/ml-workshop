0dab484dddb5c72aebb83cbcb571be2f50d74f3f
Fangfang Xia <fangfang.xia@gmail.com>
2016-07-19 15:13:39 -0500

    add pandas based csv reader

--- scripts/classify.py
+++ scripts/classify.py
@@ -1,12 +1,16 @@
 #! /usr/bin/env python
 
+from __future__ import print_function
+
 import argparse
 import errno
 import h5py
 import os
 import sys
+import time
 
 import numpy as np
+import pandas as pd
 import numpy.random
 
 from sklearn import metrics
@@ -33,11 +37,24 @@ def guess_delimiter(text):
             delim = d
     return delim
 
-def read_dataset(fname, delimiter='', skipcols=1, thresh=None, imputer=None):
-    sys.stderr.write("Reading data from {}\n".format(fname))
-    f = open(fname)
-    line = f.readline().rstrip()
-    delim = delimiter if len(delimiter) > 0 else guess_delimiter(line)
+def read_dataset_1(fname, delimiter='infer', skipcols=1, thresh=None, imputer=None):
+    sys.stderr.write("Reading data from {} ... ".format(fname))
+    time1 = time.time()
+    delim = delimiter if delimiter != 'infer' else guess_delimiter(open(fname).readline().rstrip())
+    df = pd.read_csv(fname, sep=delim, na_values=['-', 'na'], engine='c')
+    X = np.array(df.iloc[:, skipcols+1:])
+    y = np.array(df.iloc[:, skipcols:skipcols+1])
+    X_label = list(df.columns.values)[skipcols+1:]
+    sys.stderr.write("{:.2f} seconds\n".format(time.time() - time1))
+    print("Data shape:", X.shape)
+    return X, y, X_label
+
+
+def read_dataset(fname, delimiter='infer', skipcols=1, thresh=None, imputer=None):
+    sys.stderr.write("Reading data from {} ... ".format(fname))
+    time1 = time.time()
+    line = open(fname).readline()
+    delim = delimiter if delimiter != 'infer' else guess_delimiter(line.rstrip())
     cols = line.split(delim)
     # default skipcols = 1: first 2 columns (CellLine PubChemID) followed by ZScores and features
     skipcols = int(skipcols)
@@ -49,16 +66,18 @@ def read_dataset(fname, delimiter='', skipcols=1, thresh=None, imputer=None):
         imp = Imputer(missing_values='NaN', strategy=imputer, axis=0)
         X = imp.fit_transform(X)
     y = np.genfromtxt(fname, skip_header=1, delimiter=delim, usecols=[skipcols])
+    sys.stderr.write("{:.2f} seconds\n".format(time.time() - time1))
     thresh = float(thresh) if thresh else np.median(y)
     sys.stderr.write("Descritizing with threshold {}\n".format(thresh))
     y = np.transpose(map(lambda x: 1 if x > thresh else 0, y))
     return X, y, X_label
 
+
 def test():
     X, y, labels = read_dataset('toy_training.csv')
-    print X
-    print y
-    print labels
+    print(X)
+    print(y)
+    print(labels)
 
 def score_format(metric, score, eol='\n'):
     return '{:<15} = {:.5f}'.format(metric, score) + eol
@@ -127,9 +146,11 @@ def make_caffe_files(path, X, y, X2=None, y2=None):
         f.write('test.h5\n')
 
 def main():
+    start_time = time.time()
+
     parser = argparse.ArgumentParser()
     parser.add_argument('-c', '--caffe', action='store_true', help='save train and test files in HDF5 for Caffe')
-    parser.add_argument('-d', '--delimiter', default='', action='store', help='save train and test files in HDF5 for Caffe')
+    parser.add_argument('-d', '--delimiter', default='infer', action='store', help='save train and test files in HDF5 for Caffe')
     parser.add_argument('-f', '--folds', default=3, action='store', help='number of folds for cross validation if test data is not provided')
     parser.add_argument('-m', '--imputer', default=None, action='store', help='imputer to use for filling in missing values: mean, median, most_frequent')
     parser.add_argument('-o', '--outdir', action='store', help='store results files to a specified directory')
@@ -229,7 +250,9 @@ def main():
 
         sys.stderr.write('  test={:.5f} train={:.5f}\n'.format(avg_test_score, avg_train_score))
 
-    sys.stderr.write("\n")
+    end_time = time.time()
+
+    sys.stderr.write("\nTotal time: {:.1f} seconds\n\n".format(end_time - start_time))
 
 if __name__ == '__main__':
     # test()
